0db52851f77602e5c4b0191e147b8577eeb2deac
Rene Ventura <venturar729@gmail.com>
2018-12-28 02:51:58 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   # code here
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|word| alphabet.include?(word[0].downcase)}
+  arr.sort_by {|word| alphabet}
   # arr
 end
