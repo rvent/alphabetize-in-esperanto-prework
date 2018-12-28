a810d03300f5480f3a4149fb62ca33f92c26afa9
Rene Ventura <venturar729@gmail.com>
2018-12-28 03:42:07 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,13 +5,23 @@ def alphabetize(arr)
   alphabet.each_char.with_index do |char, index|
     alphabet_hash[char] = index
   end
-  short_list = arr.sort_by {|word| word.length}
-  count = 0
-  while count < short_list[0].length
-    arr.sort_by do |word|
-      alphabet_hash[word[count]]
-    end
-    count += 1
-  end
   # arr
+  helper_func(arr, alphabet_hash, 0)
+end
+
+def helper_func(arr, alphabet_hash, count)
+  new_arr = arr.sort_by do |word|
+    alphabet_hash[word[count]]
+  end
+  new_arr_sim_length = (new_arr.starts_with?(new_arr[count])).length
+  if new_arr_sim_length > 1
+    count += 1
+    new_arr[0..new_arr_sim_length] = arr[0..new_arr_sim_length].sort_by do |word|
+      alphabet_hash[word[count]]
+  end
+  if new_arr == arr
+    return arr
+  else
+    return helper_func(new_arr, alphabet_hash, count)
+  end
 end
