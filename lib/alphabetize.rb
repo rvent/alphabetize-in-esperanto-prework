deaad4d33a6ad7008d12d5391e89127bea0e1754
Rene Ventura <venturar729@gmail.com>
2018-12-28 03:06:49 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,16 +5,8 @@ def alphabetize(arr)
   alphabet.each_char.with_index do |char, index|
     alphabet_hash[char] = index
   end
-  arr.sort_by do |first, next|
-    count = 0
-    while true
-      if alphabet_hash[first[count]] == alphabet_hash[next[count]]
-        count += 1
-      else
-        alphabet_hash[first[count]] <=> alphabet_hash[next[count]]
-        break
-      end
-    end
+  arr.sort_by do |first|
+    alphabet_hash[first[0]]
   end
   # arr
 end
