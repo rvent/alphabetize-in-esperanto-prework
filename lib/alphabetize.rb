43db885a3606e6898be9f6fda165a731c0834688
Rene Ventura <venturar729@gmail.com>
2018-12-28 03:56:17 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -16,6 +16,7 @@ def helper_func(arr, alphabet_hash, count)
   new_arr_sim = new_arr.select {|word| word.start_with?word[0]}
   #p new_arr, new_arr_sim
   new_arr_sim_length = new_arr_sim.length
+  p new_arr_sim_length
   if new_arr_sim_length > 1
     count += 1
     p new_arr[0...new_arr_sim_length]
