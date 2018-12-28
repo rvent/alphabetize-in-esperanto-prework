06d55f5a32fa076439b8c1e816e1e99eda146000
Rene Ventura <venturar729@gmail.com>
2018-12-28 04:05:25 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -6,30 +6,35 @@ def alphabetize(arr)
     alphabet_hash[char] = index
   end
   # arr
-  helper_func(arr, alphabet_hash, 0)
+  new_arr = arr.sort_by do |word|
+    alphabet_hash[word[0..-1]
+  end
 end
 
-def helper_func(arr, alphabet_hash, count)
-  new_arr = arr.sort_by do |word|
-    alphabet_hash[word[count]]
-  end
-  new_arr_sim = new_arr.select {|word| word.start_with?(new_arr[count][0])}
-  p new_arr, new_arr_sim
-  new_arr_sim_length = new_arr_sim.length
-  p new_arr_sim_length
-  if new_arr_sim_length > 1
-    count += 1
-    p new_arr[0...new_arr_sim_length]
-    new_arr[0...new_arr_sim_length] = arr[0...new_arr_sim_length].sort_by do |word|
-      alphabet_hash[word[count]]
-    end
-  end
-  if new_arr == arr
-    return arr
-  else
-    return helper_func(new_arr, alphabet_hash, count)
-  end
-end
+# def helper_func(arr, alphabet_hash, count)
+#   new_arr = arr.sort_by do |word|
+#     alphabet_hash[word[count]]
+#   end
+#   new_arr_sim = new_arr.select {|word| word.start_with?(new_arr[count][0])}
+#   p new_arr, new_arr_sim
+#   new_arr_sim_length = new_arr_sim.length
+#   p new_arr_sim_length
+#   while new_arr_sim_length > 1
+#     count += 1
+#     new_arr_sim = new_arr.select {|word| word.start_with?(new_arr[count][0])}
+#     new_arr_sim_length = new_arr_sim.length
+#     p new_arr_sim_length
+#     p new_arr[0...new_arr_sim_length]
+#     new_arr[0...new_arr_sim_length] = arr[0...new_arr_sim_length].sort_by do |word|
+#       alphabet_hash[word[count]]
+#     end
+#   end
+#   if new_arr == arr
+#     return arr
+#   else
+#     return helper_func(new_arr, alphabet_hash, count)
+#   end
+# end
 
 # q = ["hi"]
 # w = ["hi", "hello"]
