a30a98875028b2730f7b5283dc3de1e8e5006fdb
Rene Ventura <venturar729@gmail.com>
2018-12-28 02:54:05 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,12 @@
 def alphabetize(arr)
   # code here
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|word| alphabet}
+  arr.sort_by do |word|
+    alphabet.each_char.with_index do |char, i|
+      if word[0] == char
+        i
+      end
+    end
+  end
   # arr
 end
